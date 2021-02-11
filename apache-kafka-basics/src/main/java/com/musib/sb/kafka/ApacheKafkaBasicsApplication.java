6188d8fe0c44035786b989d7dbdf6f053dd51bdb
musibs <musib.somnath@gmail.com>
2021-02-11 14:14:18 +0530

    Kafka Basics

---
+++ apache-kafka-basics/src/main/java/com/musib/sb/kafka/ApacheKafkaBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.musib.sb.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaBasicsApplication.class, args);
+	}
+
+}
---
+++ apache-kafka-basics/src/test/java/com/musib/sb/kafka/ApacheKafkaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musib.sb.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
